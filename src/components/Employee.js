988c00915a8be2246ab70f0277b69b197f4b2f37
aliceauko <alice.auko@student.moringaschool.com>
2022-09-06 16:03:43 +0300

    Add functions to the Employee component to hand update and delete of data

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,43 @@
 import React from 'react'
 
-function Employee() {
+function Employee({employee, onUpdateEmployee, onDeleteEmployee}) {
+    function handleDeleteClick(){
+        fetch(`http://localhost:9292/employee/${employee.id}`, {
+            method: "DELETE",
+          })
+            .then((r) => r.json())
+            .then(() => onDeleteEmployee(employee));
+        }
+        function handleIsPresentClick() {
+            fetch(`http://localhost:9292/employee/${employee.id}`, {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                is_present: !employee.is_present,
+              }),
+            })
+              .then((r) => r.json())
+              .then((updatedEmployee) => onUpdateEmployee(updatedEmployee));
+          }
+
+
   return (
-    <div>
-      
-    </div>
+    <li className={item.isInCart ? "in-cart" : ""}>
+    <span>{employee.name}</span>
+    <span className="category">{employee.category}</span>
+    <button
+      className={item.isInCart ? "remove" : "add"}
+      onClick={handleIsPresentClick}
+    >
+      {employee.is_present ? "Remove From" : "Add to"} Cart
+    </button>
+    <button className="remove" onClick={handleDeleteClick}>
+      Delete
+    </button>
+  </li>
+    
   )
 }
 
