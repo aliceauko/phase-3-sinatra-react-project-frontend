f97cb758bce297ff47b06a46bea8a274faef9c49
aliceauko <alice.auko@student.moringaschool.com>
2022-09-06 15:50:36 +0300

    Add the dark mode to the header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+function Header({ isDarkMode, onDarkModeClick }) {
+    return (
+      <header>
+        <h2>Employee Management System</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+    );
+  }
 
 export default Header
