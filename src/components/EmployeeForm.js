74a72bfdec1be5ad25cf428f47ee81e54c2db1b5
aliceauko <alice.auko@student.moringaschool.com>
2022-09-06 16:16:16 +0300

    Fetch employee data and set a form in the EmployeeList component to add new entry

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function EmployeeForm() {
+function EmployeeForm({onAddEmployee}) {
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("Director");
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const employeeData = {
+            name: name,
+            category: category,
+            is_present: false,
+        };
+        fetch("http://localhost:9292/employee", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(employeeData),
+    })
+      .then((r) => r.json())
+      .then((newEmployee) => onAddEmployee(newEmployee));
+
+
+
+    }
   return (
-    <div>
-      
-    </div>
+    <form className="NewItem" onSubmit={handleSubmit}>
+    <label>
+      Name:
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </label>
+
+    <label>
+      Category:
+      <select
+        name="category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="Director">Produce</option>
+        <option value="Secretary">Dairy</option>
+        <option value="Data">Dessert</option>
+      </select>
+    </label>
+
+    <button type="submit">Add to List</button>
+  </form>
+   
   )
 }
 
