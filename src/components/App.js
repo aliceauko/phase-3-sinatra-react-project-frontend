8af711dabd1b18501e1d0e2b928391fdfa71c3cb
aliceauko <alice.auko@student.moringaschool.com>
2022-09-06 15:41:02 +0300

    Add the light and dark mode to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import TotalList from "./TotalList";
+import Header from "./Header";
 
 function App() {
-  return (
-    <div>
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+  
+  return (
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <TotalList />
     </div>
   );
 }
