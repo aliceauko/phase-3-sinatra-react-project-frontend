4730674e2dcf347382fd8af722700d6b60820862
aliceauko <alice.auko@student.moringaschool.com>
2022-09-06 15:46:27 +0300

    Set up the filter function in the filkter components to filter the dat by category

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-function Filter() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+function Filter({ category, onCategoryChange }) {
+    return (
+      <div className="Filter">
+        <select
+          name="filter"
+          value={category}
+          onChange={(e) => onCategoryChange(e.target.value)}
+        >
+          <option value="All">Filter by category</option>
+          <option value="Director">Director</option>
+          <option value="Accountant">Accountant</option>
+          <option value="Data">Data</option>
+        </select>
+      </div>
+    );
+  }
 
 export default Filter
